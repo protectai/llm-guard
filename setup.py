7fb120aacb6f3bd3a4d76e6eab467bc00326d874
asofter <alwex10@gmail.com>
2023-08-20 12:05:46 +0200

    * bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def parse_requirements(file_name: str) -> List[str]:
 
 setuptools.setup(
     name="llm-guard",
-    version="0.1.0",
+    version="0.1.1",
     author="Laiyer.ai",
     author_email="hello@laiyer.ai",
     description=(
