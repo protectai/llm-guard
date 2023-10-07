76d91e31dad12741bfa19cd4f41aab0fceaa09b1
asofter <alwex10@gmail.com>
2023-10-07 15:14:04 +0200

    * fix regex on python 3.11

--- llm_guard/input_scanners/secrets_plugins/adobe.py
+++ llm_guard/input_scanners/secrets_plugins/adobe.py
@@ -17,5 +17,5 @@ class AdobeSecretDetector(RegexBasedDetector):
             r"""(?i)(?:adobe)(?:[0-9a-z\-_\t .]{0,20})(?:[\s|']|[\s|"]){0,3}(?:=|>|:{1,3}=|\|\|:|<=|=>|:|\?=)(?:'|\"|\s|=|\x60){0,5}([a-f0-9]{32})(?:['|\"|\n|\r|\s|\x60|;]|$)"""
         ),
         # Adobe Client Secret
-        re.compile(r"(?i)\b((p8e-)(?i)[a-z0-9]{32})(?:['|\"|\n|\r|\s|\x60|;]|$)"),
+        re.compile(r"(?i)\b((p8e-)[a-z0-9]{32})(?:['|\"|\n|\r|\s|\x60|;]|$)"),
     ]
