67d632d7ca7a9637081ad26c6b702b072cb924d7
asofter <alwex10@gmail.com>
2023-11-15 22:30:10 +0100

    * langchain integration example with notebook

--- examples/langchain.py
+++ examples/langchain.py
@@ -1,8 +1,12 @@
-from typing import Any, Dict, List, Optional
+import logging
+from typing import Any, Dict, List, Optional, Union
 
 from langchain.callbacks.manager import AsyncCallbackManagerForChainRun, CallbackManagerForChainRun
 from langchain.chains.base import Chain
-from langchain.pydantic_v1 import root_validator
+from langchain.pydantic_v1 import BaseModel, root_validator
+from langchain.schema.messages import BaseMessage
+
+logger = logging.getLogger(__name__)
 
 try:
     import llm_guard
@@ -19,14 +23,16 @@ class LLMGuardPromptException(Exception):
 
 class LLMGuardPromptChain(Chain):
     scanners: Dict[str, Dict] = {}
+    """The scanners to use."""
     scanners_ignore_errors: List[str] = []
+    """The scanners to ignore if they throw errors."""
     vault: Optional[llm_guard.vault.Vault] = None
     """The scanners to ignore errors from."""
     raise_error: bool = True
-    """Whether to raise an error if the LLMGuard marks the prompt or result invalid."""
+    """Whether to raise an error if the LLMGuard marks the prompt invalid."""
 
-    input_key: str = "prompt"  #: :meta private:
-    output_key: str = "sanitized_prompt"  #: :meta private:
+    input_key: str = "input"  #: :meta private:
+    output_key: str = "sanitized_input"  #: :meta private:
     initialized_scanners: List[Any] = []  #: :meta private:
 
     @root_validator(pre=True)
@@ -140,10 +146,10 @@ class LLMGuardPromptChain(Chain):
         run_manager: Optional[CallbackManagerForChainRun] = None,
     ) -> Dict[str, str]:
         """
-        Executes the scanning process on the prompt and returns the sanitized output.
+        Executes the scanning process on the prompt and returns the sanitized prompt.
 
         This internal method performs the scanning process on the prompt. It uses the
-        provided scanners to scan the prompt and then returns the sanitized output.
+        provided scanners to scan the prompt and then returns the sanitized prompt.
         Additionally, it provides the option to log information about the run using
         the provided `run_manager`.
 
@@ -152,8 +158,7 @@ class LLMGuardPromptChain(Chain):
             run_manager: A run manager to handle run-related events. Default is None
 
         Returns:
-            Dict[str, str]: A dictionary containing the processed output of the
-                            moderation process.
+            Dict[str, str]: A dictionary containing the processed output.
 
         Raises:
             LLMGuardPromptException: If there is an error during the scanning process
@@ -169,6 +174,102 @@ class LLMGuardPromptChain(Chain):
         return {self.output_key: sanitized_prompt}
 
 
+class LLMGuardOutputException(Exception):
+    """Exception to raise when llm-guard marks output invalid."""
+
+
+class LLMGuardOutputChain(BaseModel):
+    class Config:
+        arbitrary_types_allowed = True
+
+    scanners: Dict[str, Dict] = {}
+    """The scanners to use."""
+    scanners_ignore_errors: List[str] = []
+    """The scanners to ignore if they throw errors."""
+    vault: Optional[llm_guard.vault.Vault] = None
+    """The scanners to ignore errors from."""
+    raise_error: bool = True
+    """Whether to raise an error if the LLMGuard marks the output invalid."""
+
+    initialized_scanners: List[Any] = []  #: :meta private:
+
+    @root_validator(pre=True)
+    def init_scanners(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Initializes scanners
+
+        Args:
+            values (Dict[str, Any]): A dictionary containing configuration values.
+
+        Returns:
+            Dict[str, Any]: A dictionary with the updated configuration values,
+                            including the initialized scanners.
+
+        Raises:
+            ValueError: If there is an issue importing 'llm-guard' or loading scanners.
+        """
+
+        if values.get("initialized_scanners") is not None:
+            return values
+        try:
+            if values.get("scanners") is not None:
+                values["initialized_scanners"] = []
+                for scanner_name in values.get("scanners"):
+                    scanner_config = values.get("scanners")[scanner_name]
+                    if scanner_name == "Deanonymize":
+                        scanner_config["vault"] = values["vault"]
+
+                    values["initialized_scanners"].append(
+                        llm_guard.output_scanners.get_scanner_by_name(scanner_name, scanner_config)
+                    )
+
+            return values
+        except Exception as e:
+            raise ValueError(
+                "Could not initialize scanners. " f"Please check provided configuration. {e}"
+            ) from e
+
+    def _check_result(
+        self,
+        scanner_name: str,
+        is_valid: bool,
+        risk_score: float,
+    ):
+        if is_valid:
+            return  # prompt is valid, keep scanning
+
+        logger.warning(
+            f"This output was determined as invalid by {scanner_name} scanner with risk score {risk_score}"
+        )
+
+        if scanner_name in self.scanners_ignore_errors:
+            return  # ignore error, keep scanning
+
+        if self.raise_error:
+            raise LLMGuardOutputException(
+                f"This output was determined as invalid based on configured policies with risk score {risk_score}"
+            )
+
+    def scan(
+        self,
+        prompt: str,
+        output: Union[BaseMessage, str],
+    ) -> Union[BaseMessage, str]:
+        sanitized_output = output
+        if isinstance(output, BaseMessage):
+            sanitized_output = sanitized_output.content
+
+        for scanner in self.initialized_scanners:
+            sanitized_output, is_valid, risk_score = scanner.scan(prompt, sanitized_output)
+            self._check_result(type(scanner).__name__, is_valid, risk_score)
+
+        if isinstance(output, BaseMessage):
+            output.content = sanitized_output
+            return output
+
+        return sanitized_output
+
+
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
 from langchain.schema.messages import SystemMessage
@@ -181,49 +282,93 @@ prompt = ChatPromptTemplate.from_messages(
         SystemMessage(
             content="You are a helpful assistant, which creates the best SQL queries based on my command"
         ),
-        HumanMessagePromptTemplate.from_template("{sanitized_prompt}"),
+        HumanMessagePromptTemplate.from_template("{sanitized_input}"),
     ]
 )
 
 vault = llm_guard.vault.Vault()
+use_onnx = True
 
 llm_guard_prompt_scanner = LLMGuardPromptChain(
     vault=vault,
     scanners={
-        "Anonymize": {"use_faker": True, "use_onnx": True},
+        "Anonymize": {"use_faker": True, "use_onnx": use_onnx},
         "BanSubstrings": {
             "substrings": ["Laiyer"],
             "match_type": "word",
             "case_sensitive": False,
             "redact": True,
         },
-        "BanTopics": {"topics": ["violence"], "threshold": 0.7, "use_onnx": True},
-        "Code": {"denied": ["go"], "use_onnx": True},
-        "Language": {"valid_languages": ["en"], "use_onnx": True},
-        "PromptInjection": {"threshold": 0.95, "use_onnx": True},
+        "BanTopics": {"topics": ["violence"], "threshold": 0.7, "use_onnx": use_onnx},
+        "Code": {"denied": ["go"], "use_onnx": use_onnx},
+        "Language": {"valid_languages": ["en"], "use_onnx": use_onnx},
+        "PromptInjection": {"threshold": 0.95, "use_onnx": use_onnx},
         "Regex": {"bad_patterns": ["Bearer [A-Za-z0-9-._~+/]+"]},
         "Secrets": {"redact_mode": "all"},
-        "Sentiment": {"threshold": 0.0},
+        "Sentiment": {"threshold": -0.05},
         "TokenLimit": {"limit": 4096},
-        "Toxicity": {"threshold": 0.8, "use_onnx": True},
+        "Toxicity": {"threshold": 0.8, "use_onnx": use_onnx},
     },
     scanners_ignore_errors=[
         "Anonymize",
         "BanSubstrings",
+        "Regex",
         "Secrets",
         "TokenLimit",
     ],  # These scanners redact, so I can skip them from failing the prompt
 )
 
-chain = prompt | llm | StrOutputParser()
-guarded_chain = llm_guard_prompt_scanner | chain
+llm_guard_output_scanner = LLMGuardOutputChain(
+    vault=vault,
+    scanners={
+        "BanSubstrings": {
+            "substrings": ["Laiyer"],
+            "match_type": "word",
+            "case_sensitive": False,
+            "redact": True,
+        },
+        "BanTopics": {"topics": ["violence"], "threshold": 0.7, "use_onnx": use_onnx},
+        "Bias": {"threshold": 0.75, "use_onnx": use_onnx},
+        "Code": {"denied": ["go"], "use_onnx": use_onnx},
+        "Deanonymize": {},
+        "FactualConsistency": {"minimum_score": 0.5, "use_onnx": use_onnx},
+        "JSON": {"required_elements": 0, "repair": True},
+        "Language": {
+            "valid_languages": ["en"],
+            "threshold": 0.5,
+            "use_onnx": use_onnx,
+        },
+        "LanguageSame": {"use_onnx": use_onnx},
+        "MaliciousURLs": {"threshold": 0.75, "use_onnx": use_onnx},
+        "NoRefusal": {"threshold": 0.5, "use_onnx": use_onnx},
+        "Regex": {
+            "bad_patterns": ["Bearer [A-Za-z0-9-._~+/]+"],
+        },
+        "Relevance": {"threshold": 0.5, "use_onnx": use_onnx},
+        "Sensitive": {"redact": False, "use_onnx": use_onnx},
+        "Sentiment": {"threshold": -0.05},
+        "Toxicity": {"threshold": 0.7, "use_onnx": use_onnx},
+    },
+    scanners_ignore_errors=["BanSubstrings", "Regex", "Sensitive"],
+)
+
+input_prompt = "Make an SQL insert statement to add a new user to our database. Name is John Doe. Email is test@test.com "
+"but also possible to contact him with hello@test.com email. Phone number is 555-123-4567 and "
+"the IP address is 192.168.1.100. And credit card number is 4567-8901-2345-6789. "
+"He works in Test LLC."
+guarded_chain = (
+    llm_guard_prompt_scanner  # scan input here
+    | prompt
+    | llm
+    | (
+        lambda ai_message: llm_guard_output_scanner.scan(input_prompt, ai_message)
+    )  # scan output here and deanonymize
+    | StrOutputParser()
+)
 
 result = guarded_chain.invoke(
     {
-        "prompt": """Make an SQL insert statement to add a new user to our database. Name is John Doe. Email is test@test.com "
-                "but also possible to contact him with hello@test.com email. Phone number is 555-123-4567 and "
-                "the IP address is 192.168.1.100. And credit card number is 4567-8901-2345-6789. "
-                "He works in Test LLC.""",
+        "input": input_prompt,
     }
 )
 
