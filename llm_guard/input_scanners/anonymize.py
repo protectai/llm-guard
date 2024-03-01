ed6e0dba1a60a99e1a86899fb96efc68f56012d4
Oleksandr Yaremchuk <alwex10@gmail.com>
2024-03-01 16:40:34 +0100

    * improvements to the redaction in Anonymize scanner

--- llm_guard/input_scanners/anonymize.py
+++ llm_guard/input_scanners/anonymize.py
@@ -123,17 +123,21 @@ class Anonymize(Scanner):
             supported_languages=list(set(["en", language])),
         )
 
-    @staticmethod
     def _remove_conflicts_and_get_text_manipulation_data(
-        analyzer_results: List[RecognizerResult],
+        self, analyzer_results: List[RecognizerResult]
     ) -> List[RecognizerResult]:
         """
-        Remove conflicting and redundant detections from the analyzer results.
+        Iterate the list and create a sorted unique results list from it.
+
+        Only insert results which are:
+        1. Indices are not contained in other result.
+        2. Have the same indices as other results but with larger score.
+        :return: List
         """
         tmp_analyzer_results = []
         # This list contains all elements which we need to check a single result
         # against. If a result is dropped, it can also be dropped from this list
-        # since it is intersecting with another result, and we selected the other one.
+        # since it is intersecting with another result and we selected the other one.
         other_elements = analyzer_results.copy()
         for result in analyzer_results:
             other_elements.remove(result)
@@ -154,25 +158,45 @@ class Anonymize(Scanner):
                 other_elements.append(result)
                 tmp_analyzer_results.append(result)
             else:
-                LOGGER.debug("Removing element the results list due to merge", result=result)
+                LOGGER.debug(f"removing element {result} from " f"results list due to merge")
 
         unique_text_metadata_elements = []
         # This list contains all elements which we need to check a single result
         # against. If a result is dropped, it can also be dropped from this list
-        # since it is intersecting with another result, and we selected the other one.
+        # since it is intersecting with another result and we selected the other one.
         other_elements = tmp_analyzer_results.copy()
         for result in tmp_analyzer_results:
             other_elements.remove(result)
-            result_conflicted = Anonymize.__is_result_conflicted_with_other_elements(
+            result_conflicted = self.__is_result_conflicted_with_other_elements(
                 other_elements, result
             )
             if not result_conflicted:
                 other_elements.append(result)
                 unique_text_metadata_elements.append(result)
             else:
-                LOGGER.debug(
-                    "Removing element from the results list due to conflict", result=result
-                )
+                LOGGER.debug(f"removing element {result} from results list due to conflict")
+
+        # This further improves the quality of handling the conflict between the
+        # various entities overlapping. This will not drop the results insted
+        # it adjust the start and end positions of overlapping results and removes
+        # All types of conflicts among entities as well as text.
+        unique_text_metadata_elements.sort(key=lambda element: element.start)
+        elements_length = len(unique_text_metadata_elements)
+        index = 0
+        while index < elements_length - 1:
+            current_entity = unique_text_metadata_elements[index]
+            next_entity = unique_text_metadata_elements[index + 1]
+            if current_entity.end <= next_entity.start:
+                index += 1
+            else:
+                if current_entity.score >= next_entity.score:
+                    next_entity.start = current_entity.end
+                else:
+                    current_entity.end = next_entity.start
+                unique_text_metadata_elements.sort(key=lambda element: element.start)
+        unique_text_metadata_elements = [
+            element for element in unique_text_metadata_elements if element.start <= element.end
+        ]
         return unique_text_metadata_elements
 
     @staticmethod
